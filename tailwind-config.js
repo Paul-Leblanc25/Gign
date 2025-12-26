ffd2e3fd4e6754f8e47fa9ab7a0a4ea03b65e8f3
Paul-Leblanc25 <xgamerzarco4@gmail.com>
2025-12-26 02:04:04 +0100

    Add files via upload

---
+++ tailwind-config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                bspp: {
+                    navy: '#0B132B',       // Deep Navy / Bleu Nuit
+                    blue: '#1C2541',       // Secondary Blue / Bleu Ardoise Foncé
+                    light: '#3A506B',      // Lighter Blue / Bleu Gris
+                    red: '#CE2029',        // Firefighter Red / Rouge Pompier
+                    'red-dark': '#8B0000', // Dark Red
+                }
+            }
+        }
+    }
+}
